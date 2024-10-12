0212f153a35fcaacb1e47b878512e5982cf0335a
edenkozlov <96761592+edenkozlov@users.noreply.github.com>
2024-10-11 22:39:47 -0400

    t3orsome

--- src/hooks/useRoutes/useRangedRoutes.ts
+++ src/hooks/useRoutes/useRangedRoutes.ts
@@ -1,52 +1,45 @@
-import { useMemo } from 'react'
-
-//import { CURRENCY_USD, formatValue } from '@/format-crypto/format'
-
-import { useTokenPrice } from '@/hooks/queries/useTokenPrice'
-
-import type { TRouteWithMeta, TRangingRoute } from '@/views/IndexPage/Demo/components/Routes'
-import { calculateRouteTime, EVM_BASE_TOKEN_ADDRESS, getRouteByMinProperty, getTokenWorth, getUsdAmount } from './utils'
+import { useMemo } from 'react';
+import { useTokenPrice } from '@/hooks/queries/useTokenPrice';
+import type { TRouteWithMeta, TRangingRoute } from '@/views/IndexPage/Demo/components/Routes';
+import { calculateRouteTime, EVM_BASE_TOKEN_ADDRESS, getRouteByMinProperty, getTokenWorth, getUsdAmount } from './utils';
 
 function useRangedRoutes(routes: TRouteWithMeta[]) {
-  const { data: toTokenPrice } = useTokenPrice({ address: EVM_BASE_TOKEN_ADDRESS, chainId: 1 } || {})
+  const { data: toTokenPrice } = useTokenPrice({ address: EVM_BASE_TOKEN_ADDRESS, chainId: 1 } || {});
 
-  const bestItemID = routes.length === 0 ? -1 : 0
+  const bestItemID = routes.length === 0 ? -1 : 0;
   const bestTimeItemID = useMemo(
     () => getRouteByMinProperty(routes, route => calculateRouteTime(route.calculatedSteps)),
     [routes]
-  )
+  );
 
-  // Example: [2, 0, 1, 2, 3, 0, 1, 3, 4, 5] -> [2, 0, 1, 3, 4, 5]
   const routesIDList = useMemo(
     () =>
       [
         bestItemID,
-        // Place the "Fast" route in 2nd place
         ...[bestTimeItemID]
       ]
         .concat(Array.from(routes.keys()))
-        .filter(v => v >= 0) // Filter bestGasItemID and bestTimeItemID if they have a negative value
-        .filter((v, i, a) => a.indexOf(v) === i), // remove duplicates
+        .filter(v => v >= 0)
+        .filter((v, i, a) => a.indexOf(v) === i),
     [bestItemID, bestTimeItemID, routes]
-  )
+  );
 
-  // Example: [2, 0, 1, 3, 4, 5] -> [* route with id 2*, *route with id 0*, etc]
-  const formatterRouterList = useMemo(() => routesIDList.map(ID => routes[ID]), [routesIDList, routes])
+  const formatterRouterList = useMemo(() => routesIDList.map(ID => routes[ID]), [routesIDList, routes]);
 
   const rangedRoutes: TRangingRoute[] = useMemo(() => {
     if (!formatterRouterList.length) {
-      return []
+      return [];
     }
 
-    const bestItem = routes[bestItemID]
-    const bestWorth = getTokenWorth(bestItem.toTokenAmount, bestItem.calculatedSteps)
-    //const bestUSDPrice = formatValue(CURRENCY_USD, getUsdAmount(bestWorth, toTokenPrice))
+    const bestItem = routes[bestItemID];
+    const bestWorth = getTokenWorth(bestItem.toTokenAmount, bestItem.calculatedSteps);
+    const bestUSDPrice = toTokenPrice ? getUsdAmount(bestWorth, toTokenPrice) : null;
 
     return formatterRouterList.map((route) => {
-      const tokenWorth = getTokenWorth(route.toTokenAmount, route.calculatedSteps)
-      //const priceUSD = formatValue(CURRENCY_USD, getUsdAmount(tokenWorth, toTokenPrice))
-      //const loss = (Number(bestUSDPrice) - Number(priceUSD)) / (Number(bestUSDPrice) / 100)
-      const estimateTime = calculateRouteTime(route.calculatedSteps)
+      const tokenWorth = getTokenWorth(route.toTokenAmount, route.calculatedSteps).toString();
+      const priceUSD = toTokenPrice ? getUsdAmount(tokenWorth, toTokenPrice).toString() : null;
+      const loss = bestUSDPrice && priceUSD ? (Number(bestUSDPrice) - Number(priceUSD)) / (Number(bestUSDPrice) / 100) : null;
+      const estimateTime = calculateRouteTime(route.calculatedSteps);
 
       return {
         ...route,
@@ -57,15 +50,15 @@ function useRangedRoutes(routes: TRouteWithMeta[]) {
           estimateTime,
           price: {
             tokenWorth,
-            //priceUSD,
-            //loss
+            priceUSD,
+            loss
           }
         }
-      } as TRangingRoute
-    })
-  }, [formatterRouterList, bestTimeItemID, bestItemID])
+      } as TRangingRoute;
+    });
+  }, [formatterRouterList, bestTimeItemID, bestItemID, toTokenPrice]);
 
-  return rangedRoutes
+  return rangedRoutes;
 }
 
-export { useRangedRoutes }
+export { useRangedRoutes };
